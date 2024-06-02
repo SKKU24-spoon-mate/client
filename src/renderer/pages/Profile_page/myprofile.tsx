867e543650e0c2026f5c9964f79a4520ed32cba9
JooEun-Jeong <prichard147@naver.com>
2024-06-03 03:18:20 +0900

    Fix Profile style

--- src/renderer/pages/Profile_page/myprofile.tsx
+++ src/renderer/pages/Profile_page/myprofile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useRecoilValue } from 'recoil';
 
@@ -67,24 +67,27 @@ const updateProfile = async (userId: string, updatedData: Partial<UserProfileDat
 
 export const MyProfile: React.FC = () => {
   const userState = useRecoilValue(userStateAtom);
-  const location = useLocation();
-  const userId = location.state.userId;
+  console.log('userState', userState);
+  // console.log('location', location.state);
 
   const [profileData, setProfileData] = useState<UserProfileData | null>(null);
   const [openEditModal, setOpenEditModal] = useState(false);
 
-  useEffect(() => {
-    const loadProfile = async () => {
-      try {
-        const data = await fetchMyProfile(userState.userId);
+  const loadProfile = useCallback(async () => {
+    try {
+      await axios.get(baseUrl + `/profile/${userState.objectId}`).then((res) => {
+        const data = res.data;
+        console.log('data', data);
         setProfileData(data);
-      } catch (error) {
-        console.error('Failed to load profile:', error);
-      }
-    };
+      });
+    } catch (error) {
+      console.error('Failed to load profile:', error);
+    }
+  }, [userState.objectId]);
 
+  useEffect(() => {
     loadProfile();
-  }, [userId]);
+  }, []);
 
   const handleOpenEditModal = () => {
     setOpenEditModal(true);
@@ -100,7 +103,7 @@ export const MyProfile: React.FC = () => {
     status_message: string;
   }) => {
     try {
-      const updatedProfile = await updateProfile(userId, newProfileData);
+      const updatedProfile = await updateProfile(userState.objectId, newProfileData);
 
       setProfileData((prevState) => ({
         ...prevState!,
@@ -120,107 +123,112 @@ export const MyProfile: React.FC = () => {
   }
 
   return (
-    <ProfileContainer>
-      <Avatar src={LogoImage} sx={{ width: '30%', height: '30%', marginBottom: 2, mt: 15 }} />
-      <Typography variant="h5" sx={{ mb: 10, fontSize: '3rem' }}>
-        나의 프로필
-      </Typography>
-
-      <ReviewBox>
-        <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-          <Box
-            sx={{
-              textAlign: 'center',
-              mt: 5,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              marginLeft: 10,
-            }}
-          >
-            <Typography variant="body1" sx={{ fontSize: '2rem' }}>
-              매칭 횟수 x{profileData.total_mates}
-            </Typography>
-            <CircleBox sx={{ width: '10vw', height: '5vh' }}>
-              <Avatar src={spoonIcon} sx={{ width: 60, height: 60 }} />
-            </CircleBox>
-          </Box>
-          <Box
-            sx={{
-              textAlign: 'center',
-              mt: 5,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              marginrig: 10,
-            }}
-          >
-            <Typography variant="body1" sx={{ fontSize: '2rem' }}>
-              평균 숟가락 {profileData.average_spoons.toFixed(1)}
-            </Typography>
-            <SquareBox sx={{ width: 'fit-content', height: '5vh', padding: 1 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
-                {[...Array(Math.round(profileData.average_spoons))].map((_, index) => (
-                  <Avatar key={index} src={spoonIcon} sx={{ width: 60, height: 60 }} />
-                ))}
-              </Box>
-            </SquareBox>
-          </Box>
-        </Box>
-      </ReviewBox>
-
-      <Box sx={{ width: '100%', mt: 10 }}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
-          <Box></Box>
-          <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem', textAlign: 'center', marginLeft: 12 }}>
-            나의 한마디
-          </Typography>
-          <IconButton onClick={handleOpenEditModal} sx={{ display: 'flex', justifyContent: 'right' }}>
-            <SvgIcon component={MoreVertIcon} sx={{ fontSize: '70px' }} />
-          </IconButton>
-        </Box>
-        <EditProfile
-          open={openEditModal}
-          onClose={handleCloseEditModal}
-          onSave={handleSaveProfileData}
-          profileData={{
-            favorite_food: profileData.favorite_food,
-            age: profileData.age,
-            status_message: profileData.status_message,
-          }}
-        />
-
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <MyCommentBox>
-            <Typography sx={{ fontSize: '2rem', width: '70%' }}>{profileData.status_message}</Typography>
-          </MyCommentBox>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mt: 2 }}>
-          <InfoBox>
-            <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>좋아하는 음식 : {profileData.favorite_food}</Typography>
-          </InfoBox>
-          <InfoBox>
-            <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>나이 : {profileData.age}</Typography>
-          </InfoBox>
-        </Box>
-      </Box>
-
-      <Box sx={{ width: '100%', height: '100%', mt: 10 }}>
-        <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem' }}>
-          밥 친구들의 한마디
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <ProfileContainer>
+        <Avatar src={LogoImage} sx={{ width: '20%', height: '10%', marginBottom: 2, mt: 15 }} />
+        <Typography variant="h5" sx={{ mb: 10, fontSize: '3rem' }}>
+          나의 프로필
         </Typography>
-        <ScrollableCommentBox>
-          {profileData.reviews.map((review, index) => (
-            <CommentBox key={index}>
-              <UserDefaultIcon width="10%" />
-              <Typography sx={{ fontSize: '1.5rem' }}>{review.message}</Typography>
-            </CommentBox>
-          ))}
-        </ScrollableCommentBox>
-      </Box>
-
+        <ReviewBox>
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <Box
+              sx={{
+                textAlign: 'center',
+                mt: 5,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginLeft: 10,
+              }}
+            >
+              <Typography variant="body1" sx={{ fontSize: '2rem' }}>
+                매칭 횟수 x{profileData.total_mates}
+              </Typography>
+              <CircleBox sx={{ width: '10vw', height: '5vh' }}>
+                <Avatar src={spoonIcon} sx={{ width: 60, height: 60 }} />
+              </CircleBox>
+            </Box>
+            <Box
+              sx={{
+                textAlign: 'center',
+                mt: 5,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginrig: 10,
+              }}
+            >
+              <Typography variant="body1" sx={{ fontSize: '2rem' }}>
+                평균 숟가락 {profileData.average_spoons.toFixed(1)}
+              </Typography>
+              <SquareBox sx={{ width: 'fit-content', height: '5vh', padding: 1 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
+                  {[...Array(Math.round(profileData.average_spoons))].map((_, index) => (
+                    <Avatar key={index} src={spoonIcon} sx={{ width: 60, height: 60 }} />
+                  ))}
+                </Box>
+              </SquareBox>
+            </Box>
+          </Box>
+        </ReviewBox>
+        <Box sx={{ width: '100%', mt: 10 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
+            <Box></Box>
+            <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem', textAlign: 'center', marginLeft: 12 }}>
+              나의 한마디
+            </Typography>
+            <IconButton onClick={handleOpenEditModal} sx={{ display: 'flex', justifyContent: 'right' }}>
+              <SvgIcon component={MoreVertIcon} sx={{ fontSize: '70px' }} />
+            </IconButton>
+          </Box>
+          <EditProfile
+            open={openEditModal}
+            onClose={handleCloseEditModal}
+            onSave={handleSaveProfileData}
+            profileData={{
+              favorite_food: profileData.favorite_food,
+              age: profileData.age,
+              status_message: profileData.status_message,
+            }}
+          />
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <MyCommentBox>
+              <Typography sx={{ fontSize: '2rem', width: '70%' }}>{profileData.status_message}</Typography>
+            </MyCommentBox>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mt: 2 }}>
+            <InfoBox>
+              <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>좋아하는 음식 : {profileData.favorite_food}</Typography>
+            </InfoBox>
+            <InfoBox>
+              <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>나이 : {profileData.age}</Typography>
+            </InfoBox>
+          </Box>
+        </Box>
+        <Box sx={{ width: '100%', height: '100%', mt: 10 }}>
+          <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem' }}>
+            밥 친구들의 한마디
+          </Typography>
+          <ScrollableCommentBox>
+            {profileData.reviews.map((review, index) => (
+              <CommentBox key={index}>
+                <UserDefaultIcon width="10%" />
+                <Typography sx={{ fontSize: '1.5rem' }}>{review.message}</Typography>
+              </CommentBox>
+            ))}
+          </ScrollableCommentBox>
+        </Box>
+      </ProfileContainer>
       <Footer />
-    </ProfileContainer>
+    </Box>
   );
 };
 
--- src/renderer/pages/Profile_page/user_profile.tsx
+++ src/renderer/pages/Profile_page/user_profile.tsx
@@ -74,7 +74,7 @@ const UserProfile: React.FC = () => {
   useEffect(() => {
     const loadUserProfile = async () => {
       try {
-        const data = await fetchUserProfile(userId!);
+        const data = await fetchUserProfile(userId);
         setProfileData(data);
       } catch (error) {
         console.error('Failed to load user profile:', error);
@@ -121,123 +121,127 @@ const UserProfile: React.FC = () => {
   }
 
   return (
-    <ProfileContainer>
-      <Box sx={{ mt: 10 }}>
-        <UserDefaultIcon width="50%"></UserDefaultIcon>
-      </Box>
-      <Typography variant="h5" sx={{ mt: 5, mb: 5, fontSize: '2rem' }}>
-        {profileData.nickname}의 프로필
-      </Typography>
-
-      <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mb: 5 }}>
-        <Button
-          onClick={handleOpenReviewModal}
-          variant="contained"
-          sx={{
-            backgroundColor: '#477A2F',
-            '&:hover': { backgroundColor: '#477A2F' },
-            width: '20vw',
-            height: '30%',
-            fontSize: '1.5rem',
-          }}
-          disabled={reviewSubmitted}
-        >
-          숟가락 주기
-        </Button>
-        <ReviewProfile
-          open={openReviewModal}
-          onClose={handleCloseReviewModal}
-          onReviewSubmit={handleReviewSubmit}
-          userId={userId}
-        />
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: '#D9D9D9',
-            '&:hover': { backgroundColor: '#D9D9D9' },
-            color: '#000000',
-            width: '20vw',
-            height: '30%',
-            fontSize: '1.5rem',
-          }}
-          onClick={handleChatClick}
-        >
-          채팅하기
-        </Button>
-      </Box>
-
-      <ReviewBox>
-        <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
-          <Box
-            sx={{
-              textAlign: 'center',
-              mt: 5,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              marginLeft: 5,
-            }}
-          >
-            <Typography variant="body1" sx={{ fontSize: '2rem' }}>
-              매칭 횟수 x{profileData.total_mates}
-            </Typography>
-            <CircleBox sx={{ width: '10vw', height: '5vh' }}>
-              <Avatar src={spoonIcon} sx={{ width: 60, height: 60 }} />
-            </CircleBox>
-          </Box>
-          <Box sx={{ textAlign: 'center', mt: 5 }}>
-            <Typography variant="body1" sx={{ fontSize: '2rem' }}>
-              평균 숟가락 {profileData.average_spoons.toFixed(1)}
-            </Typography>
-            <SquareBox sx={{ width: 'fit-content', height: '5vh', padding: 1 }}>
-              <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
-                {[...Array(Math.round(profileData.average_spoons))].map((_, index) => (
-                  <Avatar key={index} src={spoonIcon} sx={{ width: 60, height: 60 }} />
-                ))}
-              </Box>
-            </SquareBox>
-          </Box>
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <ProfileContainer>
+        <Box sx={{ mt: 10 }}>
+          <UserDefaultIcon width="50%"></UserDefaultIcon>
         </Box>
-      </ReviewBox>
-
-      <Box sx={{ width: '100%', mt: 10 }}>
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: 2 }}>
-          <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem', textAlign: 'center' }}>
-            나의 한마디
-          </Typography>
-        </Box>
-
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <MyCommentBox>
-            <Typography sx={{ fontSize: '2rem', width: '70%' }}>{profileData.status_message}</Typography>
-          </MyCommentBox>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mt: 2 }}>
-          <InfoBox>
-            <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>좋아하는 음식 : {profileData.favorite_food}</Typography>
-          </InfoBox>
-          <InfoBox>
-            <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>나이 : {profileData.age}</Typography>
-          </InfoBox>
-        </Box>
-      </Box>
-
-      <Box sx={{ width: '100%', height: '100%', mt: 10 }}>
-        <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem' }}>
-          밥 친구들의 한마디
+        <Typography variant="h5" sx={{ mt: 5, mb: 5, fontSize: '2rem' }}>
+          {profileData.nickname}의 프로필
         </Typography>
-        <ScrollableCommentBox>
-          {profileData.reviews.map((review, index) => (
-            <CommentBox key={index}>
-              <UserDefaultIcon width="10%" />
-              <Typography sx={{ fontSize: '1.5rem' }}>{review.message}</Typography>
-            </CommentBox>
-          ))}
-        </ScrollableCommentBox>
-      </Box>
-
+        <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mb: 5 }}>
+          <Button
+            onClick={handleOpenReviewModal}
+            variant="contained"
+            sx={{
+              backgroundColor: '#477A2F',
+              '&:hover': { backgroundColor: '#477A2F' },
+              width: '20vw',
+              height: '30%',
+              fontSize: '1.5rem',
+            }}
+            disabled={reviewSubmitted}
+          >
+            숟가락 주기
+          </Button>
+          <ReviewProfile
+            open={openReviewModal}
+            onClose={handleCloseReviewModal}
+            onReviewSubmit={handleReviewSubmit}
+            userId={userId}
+          />
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#D9D9D9',
+              '&:hover': { backgroundColor: '#D9D9D9' },
+              color: '#000000',
+              width: '20vw',
+              height: '30%',
+              fontSize: '1.5rem',
+            }}
+            onClick={handleChatClick}
+          >
+            채팅하기
+          </Button>
+        </Box>
+        <ReviewBox>
+          <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <Box
+              sx={{
+                textAlign: 'center',
+                mt: 5,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginLeft: 5,
+              }}
+            >
+              <Typography variant="body1" sx={{ fontSize: '2rem' }}>
+                매칭 횟수 x{profileData.total_mates}
+              </Typography>
+              <CircleBox sx={{ width: '10vw', height: '5vh' }}>
+                <Avatar src={spoonIcon} sx={{ width: 60, height: 60 }} />
+              </CircleBox>
+            </Box>
+            <Box sx={{ textAlign: 'center', mt: 5 }}>
+              <Typography variant="body1" sx={{ fontSize: '2rem' }}>
+                평균 숟가락 {profileData.average_spoons.toFixed(1)}
+              </Typography>
+              <SquareBox sx={{ width: 'fit-content', height: '5vh', padding: 1 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
+                  {[...Array(Math.round(profileData.average_spoons))].map((_, index) => (
+                    <Avatar key={index} src={spoonIcon} sx={{ width: 60, height: 60 }} />
+                  ))}
+                </Box>
+              </SquareBox>
+            </Box>
+          </Box>
+        </ReviewBox>
+        <Box sx={{ width: '100%', mt: 10 }}>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: 2 }}>
+            <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem', textAlign: 'center' }}>
+              나의 한마디
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <MyCommentBox>
+              <Typography sx={{ fontSize: '2rem', width: '70%' }}>{profileData.status_message}</Typography>
+            </MyCommentBox>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mt: 2 }}>
+            <InfoBox>
+              <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>좋아하는 음식 : {profileData.favorite_food}</Typography>
+            </InfoBox>
+            <InfoBox>
+              <Typography sx={{ mt: 1, fontSize: '1.5rem' }}>나이 : {profileData.age}</Typography>
+            </InfoBox>
+          </Box>
+        </Box>
+        <Box sx={{ width: '100%', height: '100%', mt: 10 }}>
+          <Typography variant="body1" sx={{ mb: 1, fontSize: '2rem' }}>
+            밥 친구들의 한마디
+          </Typography>
+          <ScrollableCommentBox>
+            {profileData.reviews.map((review, index) => (
+              <CommentBox key={index}>
+                <UserDefaultIcon width="10%" />
+                <Typography sx={{ fontSize: '1.5rem' }}>{review.message}</Typography>
+              </CommentBox>
+            ))}
+          </ScrollableCommentBox>
+        </Box>
+      </ProfileContainer>
       <Footer />
-    </ProfileContainer>
+    </Box>
   );
 };
 
